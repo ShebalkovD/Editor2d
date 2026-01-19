aefa08f020afcfbd8d7f1d6dced7533f7b09cf07
denis_shebalkov <shebalkovdenis.wk@gmail.com>
2026-01-19 13:30:48 +0300

    Добавил рисование по клику

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const frame = document.querySelector('.frame')
 
+const CURRENT_COLOR = 'red'
+
 // fill frame with pixels
 const FRAME_WIDTH = 8
 const FRAME_HEIGHT = 8
@@ -10,5 +12,19 @@ frame.style.gridTemplateColumns = `repeat(${FRAME_WIDTH}, 1fr)`
 for (let i = 0; i < PIXEL_AMOUNT; i++) {
     const newPixel = document.createElement('div')
     newPixel.className = 'pixel' 
+    newPixel.setAttribute('oncontextmenu', 'return false;')
+    newPixel.addEventListener(('mousedown'), (e) => {
+        e.preventDefault()
+
+        if (e.button === 0) {
+            console.log('left')
+            e.target.style.backgroundColor = CURRENT_COLOR
+        }
+
+        if (e.button === 2) {
+            console.log('right')
+            e.target.style.backgroundColor = 'transparent'
+        }
+    })
     frame.appendChild(newPixel)
 }
