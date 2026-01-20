c0d9ee543f35212c4e4c8e3faa43553929b908e3
Denis Shebalkov <shebalkovdenis.wk@gmail.com>
2026-01-20 20:57:00 +0300

    Клик ластиком

--- js/handlers/handlePixelClick.js
+++ js/handlers/handlePixelClick.js
@@ -1,8 +1,6 @@
 import { APP_STATE } from '../main.js';
 
-export function handlePixelClick(e) {
-    e.preventDefault();
-
+function brushClick(e) {
     if (e.button === 0) {
         e.target.style.backgroundColor = APP_STATE.currentColor;
         e.target.dataset.initialBG = APP_STATE.currentColor;
@@ -11,5 +9,26 @@ export function handlePixelClick(e) {
     if (e.button === 2) {
         e.target.style.backgroundColor = 'transparent';
         e.target.dataset.initialBG = 'transparent';
+    }
+}
+
+function eraserClick(e) {
+    e.target.style.backgroundColor = 'transparent';
+    e.target.dataset.initialBG = 'transparent';
+}
+
+export function handlePixelClick(e) {
+    e.preventDefault();
+
+    switch (APP_STATE.tool) {
+        case 'brush':
+            brushClick(e);
+            break;
+        case 'eraser':
+            eraserClick(e);
+            break;
+
+        default:
+            break;
     }
 }
