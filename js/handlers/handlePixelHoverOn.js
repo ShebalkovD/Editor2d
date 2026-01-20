1334905de8d226aabaf7b3728468eaace422d536
Denis Shebalkov <shebalkovdenis.wk@gmail.com>
2026-01-20 20:58:30 +0300

    Убрал эффект наведения в режиме ластика

--- js/handlers/handlePixelHoverOn.js
+++ js/handlers/handlePixelHoverOn.js
@@ -2,6 +2,9 @@ import { APP_STATE } from '../main.js';
 
 export function handlePixelHoverOn(e) {
     e.preventDefault();
+
+    if (APP_STATE.tool === 'eraser') return;
+
     const el = e.target;
     el.dataset.initialBG = el.style.backgroundColor;
     el.style.backgroundColor = APP_STATE.currentColor;
